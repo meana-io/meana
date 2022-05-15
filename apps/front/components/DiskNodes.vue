22c304eb236cefd5b48aff43a21f00cf0daf76b6
matnaw1 <73436968+matnaw1@users.noreply.github.com>
2022-05-15 15:49:56 +0200

    Add DiskNodes

---
+++ apps/front/components/DiskNodes.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-treeview  
+    :items="servers" 
+    open-on-click
+    activatable
+    @update:active="test">
+  </v-treeview>   
+</template>
+
+
+<script>
+  export default {
+    name: 'DiskNodes',
+    
+    data() {
+      return {
+        servers: [],
+      }
+    },
+    props: {
+      apiRoute: String
+    },
+    mounted() {
+      fetch(this.apiRoute)
+      .then(res => res.json())
+      .then(data => this.servers = data)
+      .catch(err => console.log(err))
+    },
+    methods: {
+      test() {
+        console.log('TEST')
+      }
+    }
+  }
+
+</script>
