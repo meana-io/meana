b64a1ffcec3f21a1f6abc8124bcda268560cf910
matnaw1 <73436968+matnaw1@users.noreply.github.com>
2022-05-16 18:13:37 +0200

    Added DataDemo

--- apps/front/pages/index.vue
+++ apps/front/pages/index.vue
@@ -1,6 +1,10 @@
 <template>
 <v-app>
-  <DiskNodes :apiRoute="[{name: 'server 1'}, {name: 'server 2'}, {name: 'server 3'}]"/>
+  <DiskNode :apiRoute="[{id:1, name: 'server 1'}, {id:2,name: 'server 2'}, {id: 3,name: 'Home', children: [
+        {id:4,name: 'server 3'},
+        {id:5, name: 'server 4'},
+        {id:6,name: 'server 5'}
+        ]}]"/>
 </v-app>
 </template>
 
