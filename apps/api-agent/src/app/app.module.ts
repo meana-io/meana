4318e102f55419c89fd86eee8a869d6d9d741abf
dawidnowak <dawnow9@st.amu.edu.pl>
2022-06-12 20:34:45 +0200

    feat: use .env file

--- apps/api-agent/src/app/app.module.ts
+++ apps/api-agent/src/app/app.module.ts
@@ -8,18 +8,19 @@ import {Node} from "../../../api/src/app/domains/nodes/entities/node.entity";
 import {NodeDisk} from "../../../api/src/app/domains/node-disks/entities/node-disk.entity";
 import {NodeDiskPartition} from "../../../api/src/app/domains/node-disk-partitions/entities/node-disk-partition.entity";
 import {GlobalModule} from "./Domains/global/global.module";
+import {Dialect} from "sequelize";
 /* eslint-enable @nrwl/nx/enforce-module-boundaries */
 
 @Module({
   imports: [
     GlobalModule,
     SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'docker_postgres_1',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'meana',
+      dialect: process.env.DB_DIALECT as Dialect,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       models: [Node, NodeDisk, NodeDiskPartition],
     }),
   ],
--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -9,15 +9,16 @@ import {NodeDisk} from "./domains/node-disks/entities/node-disk.entity";
 import {NodeDisksModule} from "./domains/node-disks/node-disks.module";
 import {NodeDiskPartition} from "./domains/node-disk-partitions/entities/node-disk-partition.entity";
 import {NodeDiskPartitionsModule} from "./domains/node-disk-partitions/node-disk-partitions.module";
+import {Dialect} from "sequelize";
 
 @Module({
   imports: [NodesModule, NodeDisksModule, NodeDiskPartitionsModule, SequelizeModule.forRoot({
-    dialect: 'postgres',
-    host: 'docker_postgres_1',
-    port: 5432,
-    username: 'postgres',
-    password: 'password',
-    database: 'meana',
+    dialect: process.env.DB_DIALECT as Dialect,
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     models: [Node, NodeDisk, NodeDiskPartition],
   })],
   controllers: [AppController],
