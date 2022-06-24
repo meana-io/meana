6510bf7e685cd0785f5f22722615bd76cd32421a
Dawid Wesolowski <wesolowskid4@gmail.com>
2022-06-24 15:30:43 +0200

    feat: fetching ram usage every second

--- apps/front/components/Ram/index.tsx
+++ apps/front/components/Ram/index.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
 import { Grid } from '@mui/material';
 
 import ChartCard from './ChartCard';
@@ -50,13 +54,41 @@ const ramToChart = (ram: Ram[]) => {
   ];
 };
 const Ram: React.FC<RamProps> = ({ ram }) => {
+  const router = useRouter();
+  const { id: nodeId } = router.query;
+
+  console.log(nodeId);
+  const [ramUsage, setRamUsage] = useState(ram);
+
+  useEffect(() => {
+    const intervalId = setInterval(async () => {
+      const { data: nodeRam } = await axios.get(
+        'http://135.125.190.40:3333/api/node-ram',
+        {
+          data: {
+            where: {
+              nodeId,
+            },
+            linit: 50,
+          },
+        }
+      );
+
+      setRamUsage(nodeRam);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <Grid container spacing={2} direction="column">
       <Grid item xs={12} md={6}>
         <ChartCard
           title="Ram usage"
           options={RAM_USAGE_CHART_CONFIG}
-          data={ramToChart(ram)}
+          data={ramToChart(ramUsage)}
         />
       </Grid>
     </Grid>
