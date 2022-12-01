fd52843b52b7a3836ebcba53302d66ca4b0c82e6
matnaw1 <73436968+matnaw1@users.noreply.github.com>
2022-12-01 18:23:40 +0100

    add case 6 and 7

---
+++ cypress/integration/1-meana-test/AddUserV2.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+context('Check Existing Node', () => {
+  it('Check node Test-Node', () => {
+    cy.visit('https://meana.vercel.app');
+
+    cy.contains('Create').click();
+
+    cy.contains('Create User').click();
+    cy.get('#firstName').type('Jon');
+    cy.get('#lastName').type('Doe');
+    cy.get('#login').type('jondoe');
+    cy.get('#password').type('123456789');
+    cy.get('#email').type('jon@doe.com');
+    cy.contains('Submit').click();
+  });
+});
