298910ae012cb470a6155ce8af468fcab3a3df49
Rogaliczek <rogall666@gmail.com>
2023-01-06 20:47:34 +0100

    task/nowe_pola_nodecpu

--- apps/front/types/cpu.ts
+++ apps/front/types/cpu.ts
@@ -6,4 +6,22 @@ export default interface Cpu {
   manufacture?: string;
   model?: string;
   usage?: string;
+  socketDesignation?: string;
+  type?: string;
+  cpuId?: string;
+  version?: string;
+  voltage?: string;
+  externalClock?: string;
+  maxSpeed?: string;
+  status?: string;
+  upgrade?: string;
+  l1CacheHandle?: string;
+  l2CacheHandle?: string;
+  l3CacheHandle?: string;
+  serialNumber?: string;
+  assetTag?: string;
+  partNumber?: string;
+  coreEnabled?: string;
+  threadCount?: string;
+  characteristics?: string;
 }
