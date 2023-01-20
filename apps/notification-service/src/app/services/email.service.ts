1f354ef66443d2d433ffb2f7068b9a1e5ba363ad
dawidnowak <dawnow9@st.amu.edu.pl>
2023-01-20 23:32:37 +0100

    feat(users): last notification at

--- apps/notification-service/src/app/services/email.service.ts
+++ apps/notification-service/src/app/services/email.service.ts
@@ -1,15 +1,10 @@
 import { NotificationService } from './notification.service';
 import * as sgMail from '@sendgrid/mail';
-import { DateTime } from 'luxon';
+import { RuntimeException } from '@nestjs/core/errors/exceptions/runtime.exception';
 
 export class EmailService extends NotificationService {
   public send(message: string): boolean {
-    const dto = JSON.parse(message) as {
-      to: { email: string; lastNotifiedAt: string }[];
-      nodeName: string;
-      property: string;
-      actual: string;
-    };
+    const dto = JSON.parse(message);
     let sent = false;
     const apiKey = process.env.SENDGRID_APIKEY;
 
@@ -18,15 +13,8 @@ export class EmailService extends NotificationService {
     }
 
     sgMail.setApiKey(process.env.SENDGRID_APIKEY);
-
-    const users = dto.to.filter(
-      (user) =>
-        DateTime.fromISO(user.lastNotifiedAt).plus({ minute: 5 }) <
-        DateTime.now()
-    );
-
     const msg = {
-      to: users,
+      to: dto.to,
       from: 'noreply@meana.ovh',
       subject: 'MEANA - status of exceeding the allowed thresholds',
       text: `Node: ${dto.nodeName} \r\n Exceed ${dto.property} with value ${dto.actual}`,
